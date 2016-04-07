b6df207f137d1f48d596aff03c93eba15c1f3ee5
TheFling <Frej_95@live.com>
2016-04-07 13:47:43 +0200

    Main menu
    Added

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+	// Use this for initialization
+
+	public void StartGame () 
+    {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	
+	}
+	
+    public void QuitGame()
+    {
+        Debug.Log("The game Quit");
+        Application.Quit();
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
