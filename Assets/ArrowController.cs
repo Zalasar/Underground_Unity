b5c89f8c1141c85b8b99be088955ddce7b6c91c7
Zalasar <xalaxar@hotmail.com>
2016-04-11 23:03:45 +0200

    Updated sprites and arrow-mech

--- Assets/ArrowController.cs
+++ Assets/ArrowController.cs
@@ -5,6 +5,7 @@ public class ArrowController : MonoBehaviour {
     public int damage = 5;
 
     public float speed;
+    public float fallBoundry = -20;
 
     public Rigidbody2D rb;
 	void Start () {
@@ -14,7 +15,11 @@ public class ArrowController : MonoBehaviour {
 	void Update ()
     {
         rb.velocity = new Vector2(speed, rb.velocity.y);
-	}
+        if (rb.position.y <= fallBoundry)
+        {
+            Destroy(gameObject);
+        }
+    }
     void OnTriggerEnter2D(Collider2D col)
     {
         col.SendMessageUpwards("Damage", damage);
