9482417413c51312d4980ff6098288b98beba079
Zalasar <xalaxar@hotmail.com>
2016-04-25 23:23:09 +0200

    LootDrops som borde funka?

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class Enemy : MonoBehaviour {
     float currentHealth = 10;
     float maxHealth = 100;
+
+    public EnemyLootDrop lootSpawner;
+    //static ItemDataBaseList inventoryItemList;
 //	[System.Serializable]
  //   public class EnemyStats
  //   {
@@ -22,12 +25,14 @@ public class Enemy : MonoBehaviour {
  public void Awake()
     {
         currentHealth = maxHealth;
+        //inventoryItemList = (ItemDataBaseList)Resources.Load("ItemDatabase");
     }
  public void Update()
     {
         if (currentHealth <=0)
         {
             Destroy(gameObject);
+            lootSpawner.DropLoot(new Vector2(this.transform.position.x, this.transform.position.y), 1);
         }
     }
  public void Damage(int damage)
---
+++ Assets/EnemyLootDrop.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyLootDrop : MonoBehaviour {
+    static ItemDataBaseList inventoryItemList;
+    // Use this for initialization
+    void Start ()
+    {
+        inventoryItemList = (ItemDataBaseList)Resources.Load("ItemDatabase");
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+    public void DropLoot(Vector2 dropPoint, int whatItem)
+    {
+        GameObject lootItem = (GameObject)Instantiate(inventoryItemList.itemList[whatItem].itemModel, dropPoint, Quaternion.Euler(0,0,0));
+        PickUpItem item = lootItem.AddComponent<PickUpItem>();
+        item.item = inventoryItemList.itemList[whatItem];
+        lootItem.transform.localPosition = dropPoint;
+    }
+}
