005356f805861315546d2a2b705b674585b52749
Zalasar <xalaxar@hotmail.com>
2016-05-04 12:02:34 +0200

    Boss Stuff WIP

---
+++ Assets/BossAI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossAI : MonoBehaviour {
+
+    Transform target;
+	// Use this for initialization
+	void Awake () 
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        Vector2 direction = new Vector2(target.localPosition.x - transform.localPosition.x, target.localPosition.y - transform.localPosition.y);
+        if (direction.sqrMagnitude < 2)
+        {
+            
+        }
+	}
+}
