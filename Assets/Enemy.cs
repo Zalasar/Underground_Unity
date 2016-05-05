ac5cc384f8b2a37b1f074cf5a2c566ea52bdb8be
TheFling <Frej_95@live.com>
2016-05-05 13:45:07 +0200

    The healthbar works and is totally not broken

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -3,8 +3,27 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour
 {
-    float currentHealth = 10;
-    float maxHealth = 100;
+
+    [System.Serializable]
+    public class EnemyStats
+    {
+        public float maxHealth = 100;
+
+        private float _curhealth=10;
+        public float currentHealth
+        {
+            get { return _curhealth; }
+            set { _curhealth = Mathf.Clamp(value, 0, maxHealth);  }
+
+        }
+
+        public void Init()
+        {
+            currentHealth = maxHealth;
+            Debug.Log(currentHealth);
+        }
+    
+    }
 
     public EnemyLootDrop lootSpawner;
     //static ItemDataBaseList inventoryItemList;
@@ -36,21 +55,35 @@ public class Enemy : MonoBehaviour
         //Destroy(gameObject);
         //Destroy(other.gameObject);
     }
-    public void Awake()
+    public EnemyStats stats = new EnemyStats();
+
+    [Header("Optional: ")]
+    [SerializeField]
+    private StatusIndicator statusIndicator;
+
+    void Start()
     {
-        currentHealth = maxHealth;
-        //inventoryItemList = (ItemDataBaseList)Resources.Load("ItemDatabase");
+        stats.Init();   
+        
+        if (statusIndicator != null)
+        {
+            statusIndicator.SetHealth(stats.currentHealth, stats.maxHealth);
+        }
     }
     public void Update()
     {
-        if (currentHealth <= 0)
+        if (stats.currentHealth <= 0)
         {
             Destroy(gameObject);
             lootSpawner.DropLoot(new Vector2(this.transform.position.x, this.transform.position.y), 1);
         }
+        if (statusIndicator != null)
+        {
+            statusIndicator.SetHealth(stats.currentHealth, stats.maxHealth);
+        }
     }
     public void Damage(int damage)
     {
-            currentHealth -= damage;
+            stats.currentHealth -= damage;
     }
 }
---
+++ Assets/StatusIndicator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StatusIndicator : MonoBehaviour {
+
+    [SerializeField]
+    private RectTransform healthBarRect;
+
+    void Start()
+    {
+        if(healthBarRect == null)
+        {
+            Debug.Log("no healthbar");
+        }
+    }
+	public  void SetHealth(float curr, float max)
+    {
+        float value = (float) curr / max;
+
+        healthBarRect.localScale =new Vector3(value, healthBarRect.localScale.y, healthBarRect.localScale.z);
+    }
+}
