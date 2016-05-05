c1ca9655334850b7d78d30b2b354ac826f0e8e7c
Zalasar <xalaxar@hotmail.com>
2016-05-05 13:45:38 +0200

    Boss Base

--- Assets/BossAI.cs
+++ Assets/BossAI.cs
@@ -1,22 +1,53 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class BossAI : MonoBehaviour {
 
-    Transform target;
+    public Transform target;
+    public Rigidbody2D rb;
+    private float timer;
+    float speed;
 	// Use this for initialization
 	void Awake () 
     {
-	    
+        speed = 3;
+        timer = 5;
+        rb = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
-        Vector2 direction = new Vector2(target.localPosition.x - transform.localPosition.x, target.localPosition.y - transform.localPosition.y);
-        if (direction.sqrMagnitude < 2)
+        
+        Vector2 direction = new Vector2(target.position.x - transform.position.x, target.position.y - transform.position.y);
+        Debug.Log(direction);
+        //Vector2 distance = new Vector2(Mathf.Abs(target.position.x))
+        //Vector2 distance = Vector2.Distance(target.position., transform.TransformVector);
+        //direction.Normalize();
+        Debug.Log(direction.sqrMagnitude);
+        if (direction.sqrMagnitude < 70)
         {
-            
+            timer -= Time.deltaTime;
+            direction.Normalize();
+            rb.velocity -= direction * Time.deltaTime * speed;
+            if (timer <= 0)
+            {
+                rb.velocity = new Vector2(Random.Range(0, 4), Random.Range(0, 4));
+                timer = 3;
+                
+            }
         }
+        else
+        {
+            direction.Normalize();
+            rb.velocity += direction * Time.deltaTime;
+        }
+        
+        //if (timer <= 0)
+        //{
+        //    rb.velocity = new Vector2(Random.Range(0, 4), Random.Range(0, 4));
+        //    timer = 3;
+        //}
 	}
 }
