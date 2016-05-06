57ccb25ddd83db029b15209b600cfac8e6415a11
TheFling <Frej_95@live.com>
2016-05-06 13:14:02 +0200

    It's sparkly clean

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -26,34 +26,19 @@ public class Enemy : MonoBehaviour
     }
 
     public EnemyLootDrop lootSpawner;
-    //static ItemDataBaseList inventoryItemList;
+    
     float damage = 10;
-    //	[System.Serializable]
-    //   public class EnemyStats
-    //   {
-    //       public int Health = 100;
-    //   }
-    //   public EnemyStats stats = new EnemyStats();
-
-    //   public void DamageEnemy (int Damage)
-    //   {
-    //       stats.Health -= Damage;
-    //       if(stats.Health <= 0)
-    //       {
-    //           GameMaster.KillEnemy(this);
-    //       }
-    //   }
+  
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
             other.SendMessageUpwards("Damage", damage);
-            //(Damage()
+            
         }
         Debug.Log("Hit");
-        //Destroy(gameObject);
-        //Destroy(other.gameObject);
+        
     }
     public EnemyStats stats = new EnemyStats();
 
