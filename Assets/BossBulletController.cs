75a6df2dc6d3d0131a453d5a5b5e94ab2544f6f7
Zalasar <xalaxar@hotmail.com>
2016-05-06 12:01:42 +0200

    Okay Boss I guess

--- Assets/BossBulletController.cs
+++ Assets/BossBulletController.cs
@@ -1,40 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ArrowController : MonoBehaviour {
-    public int damage = 5;
+public class BossBulletController : MonoBehaviour {
 
+    public int damage = 5;
     public float speed;
-    public float fallBoundry = -20;
-    public float Timer = 2;
-    
+    public float Timer = 10;
+
     public Rigidbody2D rb;
-	void Start ()
+    void Start()
     {
         rb.velocity = new Vector2(speed * rb.velocity.x, speed * rb.velocity.y);
     }
-	
-	void Update ()
+
+    void Update()
     {
         Timer -= Time.deltaTime;
         if (Timer <= 0)
         {
             Destroy(gameObject);
-            Timer = 2;
+            Timer = 10;
         }
     }
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Enemy")
+        if (other.tag == "Player")
         {
             other.SendMessageUpwards("Damage", damage);
             Destroy(gameObject);
-
         }
-        else if (other.tag != "Enemy")
+        else
         {
             Destroy(gameObject);
         }
-        //Destroy(other.gameObject);
     }
 }
